42ec2d24be6d823b5b89c5d4f0741fd85ede0402
Amine MBARKI <a.mbarki@dm73.net>
2015-03-04 10:13:35 +0000

    init project

---
+++ puphpet/files/exec-always/00-init-project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# cd /var/www/domain.dev && \
+#  npm install --no-bin-links && \
+#  php composer.phar install && \
+#  php app/console doctrine:schema:update --force
---
+++ puphpet/files/startup-once/00-setup-compass.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install Compass & plugins
+rvmsudo gem install compass
+rvmsudo gem install autoprefixer-rails
+
+# Install Composer
+# cd /var/www/domain.dev && curl -sS https://getcomposer.org/installer | php
